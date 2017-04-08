d7617572ed7e5d1cebc8c3427fda24d0834a78d7
Blake Feggans <bfeggans@gmail.com>
2017-04-08 12:22:23 -0700

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent }  from './app.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroesComponent }  from './heroes.component';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroService } from './hero.service';
+import { HeroService }          from './hero.service';
 
 import { AppRoutingModule } from './app-routing.module'
 
@@ -14,6 +19,8 @@ import { AppRoutingModule } from './app-routing.module'
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
